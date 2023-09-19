1e33c1ea0be6a5c23f6f730c7e8dbb1f601d5045
SalimataAzzolini <salimataazzolini@gmail.com>
2023-09-19 16:57:14 +0200

    ✨ F05 : MainOrder > Add grid layout

--- src/pages/order/MainOrder.jsx
+++ src/pages/order/MainOrder.jsx
@@ -5,7 +5,8 @@ export default function MainOrder() {
 
   return (
     <MainOrderStyled>
-        MainOrder
+        <div className="basket"> Basket </div>
+        <div className="menu"> Menu </div>
     </MainOrderStyled>
   )
 }
@@ -18,5 +19,15 @@ const MainOrderStyled = styled.div`
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
+
+    display: grid;
+    grid-template-columns: 25% 1fr;
+
+    .basket{
+      background: #efa1a1;
+    }
+    .menu{
+      background: #632657; 
+    }
         
 `;
