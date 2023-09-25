cdc46ec39a6a865e966d811de97f689558cd3015
SalimataAzzolini <salimataazzolini@gmail.com>
2023-09-25 11:06:37 +0200

    🎨 F06 : ToastAdmin > remove code and improve it

--- src/pages/order/Navbar/ToastAdmin.jsx
+++ src/pages/order/Navbar/ToastAdmin.jsx
@@ -4,30 +4,27 @@ import styled from 'styled-components';
 import { theme } from '../../../theme';
 
 export default function ToastAdmin() {
-  return (
-    <ToastAdminStyled>
-        <ToastContainer className="toaster" bodyClassName="body-toast" />
-    </ToastAdminStyled>
-  )
+  return  <ToastContainerStyled className="toaster" bodyClassName="body-toast" />
+  
 }
 
-const ToastAdminStyled = styled.div`
+const ToastContainerStyled = styled(ToastContainer)`
   
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
+    .toaster {
+        max-width: 300px;
     }
-    div {
-      line-height: 1.3em;
+
+    .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+        background: ${theme.colors.background_dark};
     }
-  }
+
+    .body-toast {
+        .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+            margin-right: 20px;
+            margin-left: 5px;
+        }
+        div {
+            line-height: 1.3em;
+        }
+    }
 `;
