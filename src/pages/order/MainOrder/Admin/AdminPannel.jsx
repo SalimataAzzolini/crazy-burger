9df2fbb0a198ed865be6bfd8822b96454292c3ce
SalimataAzzolini <salimataazzolini@gmail.com>
2023-09-28 13:01:06 +0200

    F07 : ADMIN > Add tabsConfig

--- src/pages/order/MainOrder/Admin/AdminPannel.jsx
+++ src/pages/order/MainOrder/Admin/AdminPannel.jsx
@@ -3,26 +3,21 @@ import { theme } from "../../../../theme";
 import { useContext } from "react";
 import OrderContext from "../../../../context/OrderContext";
 
-
 export default function AdminPannel() {
-
-  const { isAddSelected, isEditSelected } = useContext (OrderContext);
-
+  const { isAddSelected, isEditSelected } = useContext(OrderContext);
 
   return (
-    <AdminPannelStyled >
-        {isAddSelected && <div>Ajouter un produit</div>}
-        {isEditSelected && <div> Modifier un produit </div>}
+    <AdminPannelStyled>
+      {isAddSelected && <div>Ajouter un produit</div>}
+      {isEditSelected && <div> Modifier un produit </div>}
     </AdminPannelStyled>
-  )
+  );
 }
 
-
 const AdminPannelStyled = styled.div`
-  
-    border: 2px solid green;
-    height: 250px;
-    background: ${theme.colors.white};
-    border : 1px solid ${theme.colors.greyLight};
-    box-shadow: ${theme.shadows.subtle};
+  border: 2px solid green;
+  height: 250px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
 `;
--- src/pages/order/MainOrder/Admin/AdminTabs.jsx
+++ src/pages/order/MainOrder/Admin/AdminTabs.jsx
@@ -1,77 +1,78 @@
 import styled from "styled-components";
 import Tab from "../../../../components/reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
-import {AiOutlinePlus} from "react-icons/ai";
-import {MdModeEditOutline} from "react-icons/md";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 import { theme } from "../../../../theme";
 import { useContext } from "react";
 import OrderContext from "../../../../context/OrderContext";
 
-
-
-
 export default function AdminTabs() {
+  const {
+    isCollapsed,
+    setIsCollapsed,
+    isAddSelected,
+    setIsAddSelected,
+    isEditSelected,
+    setIsEditSelected,
+  } = useContext(OrderContext);
 
-    const {
-            isCollapsed, setIsCollapsed,
-            isAddSelected, setIsAddSelected,
-            isEditSelected, setIsEditSelected
-          } = useContext(OrderContext);
-  
+  const selectTab = (tab) => {
+    setIsCollapsed(false);
+    if (tab === "add") {
+      setIsAddSelected(true);
+      setIsEditSelected(false);
+    } else if (tab === "edit") {
+      setIsEditSelected(true);
+      setIsAddSelected(false);
+    }
+  };
 
-    const selectTab = (tab) => {
-
-        setIsCollapsed(false);
-        if(tab === 'add') {
-          setIsAddSelected(true);
-          setIsEditSelected(false);
-          
-        } else if(tab === 'edit') {
-          setIsEditSelected(true);
-          setIsAddSelected(false);
-        }
-  }
-
-
-  
+  const tabsConfig = [
+    {
+      index: "chevronUpDown",
+      label: "",
+      Icon: isCollapsed ? <FiChevronUp /> : <FiChevronDown />,
+      onClickTab: () => setIsCollapsed(!isCollapsed),
+      className: isCollapsed ? "is-active" : "",
+    },
+    {
+      index: "add",
+      label: "Ajouter un produit",
+      Icon: <AiOutlinePlus />,
+      onClickTab: () => selectTab("add"),
+      className: isAddSelected ? "is-active" : "",
+    },
+    {
+      index: "edit",
+      label: "Modifier un produit",
+      Icon: <MdModeEditOutline />,
+      onClickTab: () => selectTab("edit"),
+      className: isEditSelected ? "is-active" : "",
+    },
+  ];
 
   return (
-    <AdminTabsStyled >
-        <Tab
-            label=""
-            Icon={ isCollapsed ? <FiChevronUp/> : <FiChevronDown/> }
-            onClickTab={() => setIsCollapsed(!isCollapsed)}
-            className={isCollapsed ? "is-active" : ""}
-        />
-
-        <Tab
-            label="Ajouter un produit"
-            Icon={ <AiOutlinePlus/> }
-            onClickTab={() => selectTab('add') }
-            className={isAddSelected ? "is-active" : ""}
-        />
-        <Tab
-            label="Modifier un produit"
-            Icon={ <MdModeEditOutline/> }
-            onClickTab={() => selectTab('edit') }
-            className={isEditSelected ? "is-active" : ""}
-        />
+    <AdminTabsStyled>
+      {tabsConfig.map((tab, index) => (
+        <Tab key={index} {...tab} />
+      ))}
     </AdminTabsStyled>
-  )
+  );
 }
 
 const AdminTabsStyled = styled.div`
-    /* border : solid 2px green; */
-    display: flex;
-    padding: 0 20px;
+  /* border : solid 2px green; */
+  display: flex;
+  padding: 0 20px;
 
-    .is-active{
-      background: ${theme.colors.background_dark};
-      color: ${theme.colors.white};
-      border-color: ${theme.colors.white};
-    }
+  .is-active {
+    background: ${theme.colors.background_dark};
+    color: ${theme.colors.white};
+    border-color: ${theme.colors.white};
+  }
 
-    button{
-      margin-left: 1px;
-    }
+  button {
+    margin-left: 1px;
+  }
 `;
