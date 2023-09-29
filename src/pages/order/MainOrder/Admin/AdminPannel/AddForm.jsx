ed25cde694eece7602bf68e952364b909ba65463
SalimataAzzolini <salimataazzolini@gmail.com>
2023-09-29 11:26:02 +0200

    F08 : ADMIN Pannel > addForm : input layout

--- src/pages/order/MainOrder/Admin/AdminPannel/AddForm.jsx
+++ src/pages/order/MainOrder/Admin/AdminPannel/AddForm.jsx
@@ -1,7 +1,43 @@
 import styled from "styled-components";
 
 export default function AddForm() {
-  return <AddFormStyled>AddForm</AddFormStyled>;
+  return (
+    <AddFormStyled>
+      <div className="image-preview"> img prev</div>
+      <div className="input-fields">
+        <input type="text" placeholder="Nom du produit" />{" "}
+        <input type="text" placeholder="Image URL" />{" "}
+        <input type="text" placeholder="Price" />{" "}
+      </div>
+      <div className="submit-button"> btn </div>
+    </AddFormStyled>
+  );
 }
 
-const AddFormStyled = styled.div``;
+const AddFormStyled = styled.form`
+  border: 4px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+
+  height: 100%;
+  width: 70%;
+
+  .image-preview {
+    background: red;
+    grid-area: 1 / 1 / 4 / 2;
+  }
+
+  .input-fields {
+    background: blue;
+    grid-area: 1 / 2 / -2 / -1;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  .submit-button {
+    background: green;
+    grid-area: 4 / 2 / 5 / 3;
+  }
+`;
