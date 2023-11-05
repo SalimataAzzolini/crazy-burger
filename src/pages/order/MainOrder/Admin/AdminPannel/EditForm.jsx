a774547bac857a8653fed51c2205d3caa18f07ee
SalimataAzzolini <salimataazzolini@gmail.com>
2023-11-05 20:23:22 +0100

    ♻️ F09 : Admin > EditForm, AddForm creation of a reusable component Form for this two components

--- src/pages/order/MainOrder/Admin/AdminPannel/EditForm.jsx
+++ src/pages/order/MainOrder/Admin/AdminPannel/EditForm.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-// import HintMessage from "./HintMessage";
+
 import { useContext } from "react";
 import OrderContext from "../../../../../context/OrderContext";
-import TextInput from "../../../../../components/reusable-ui/TextInput";
-import ImagePreview from "./ImagePreview";
-import { getInputTextsConfig } from "./InputTextConfig";
-import { theme } from "../../../../../theme";
+import EditInfoMessage from "./EditInfoMessage";
+import Form from "./Form";
 
-const EditForm = React.forwardRef(function EditForm(props, ref) {
+const EditForm = React.forwardRef(function EditForm() {
   //ici on utilise le forwardRef pour pouvoir utiliser la ref dans le composant parent
   const {
     productSelected,
@@ -16,8 +13,6 @@ const EditForm = React.forwardRef(function EditForm(props, ref) {
     handleEditProduct,
     titleEditRef,
   } = useContext(OrderContext);
-  //input texts config
-  const inputTexts = getInputTextsConfig(productSelected);
 
   // comportements (gestionnaires d'événement:  "event handlers")
   const handleChange = (event) => {
@@ -30,65 +25,10 @@ const EditForm = React.forwardRef(function EditForm(props, ref) {
   };
 
   return (
-    <EditFormStyled>
-      <ImagePreview
-        imageSource={productSelected.imageSource}
-        title={productSelected.title}
-      />
-      <div className="input-fields">
-        {inputTexts.map((input) => (
-          <TextInput
-            {...input}
-            key={input.id}
-            onChange={handleChange}
-            version="minimalist"
-            ref={titleEditRef && input.name === "title" ? titleEditRef : null}
-          />
-        ))}
-      </div>
-      <div className="submit">
-        <span className="sentence">
-          Cliquer sur un produit du menu pour le modifier{" "}
-          <span className="live-update">en temps réel</span>
-        </span>
-      </div>
-    </EditFormStyled>
+    <Form product={productSelected} onChange={handleChange} ref={titleEditRef}>
+      <EditInfoMessage />
+    </Form>
   );
 });
 
 export default EditForm;
-
-const EditFormStyled = styled.form`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: repeat(4, 1fr);
-  grid-column-gap: 20px;
-  grid-row-gap: 8px;
-
-  height: 100%;
-  width: 70%;
-
-  .input-fields {
-    grid-area: 1 / 2 / -2 / -1;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(3, 1fr);
-    grid-row-gap: 8px;
-  }
-
-  .submit {
-    grid-area: 4 / -2 / -1 / -1;
-    display: flex;
-    align-items: center;
-    position: relative;
-    top: 3px;
-
-    .sentence {
-      color: ${theme.colors.primary};
-      font-size: ${theme.fonts.size.SM};
-      .live-update {
-        text-decoration: underline;
-      }
-    }
-  }
-`;
