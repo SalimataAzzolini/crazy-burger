6b66c07becfca9659297b23bd54bd912064c5f66
SalimataAzzolini <salimataazzolini@gmail.com>
2023-11-20 21:03:06 +0100

    ♻️ F10 : ADMIN > Refactor AddForm component and use useSuccessMessage hook

--- src/pages/order/MainOrder/Admin/AdminPannel/AddForm.jsx
+++ src/pages/order/MainOrder/Admin/AdminPannel/AddForm.jsx
@@ -1,16 +1,17 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import OrderContext from "../../../../../context/OrderContext";
 import { EMPTY_PRODUCT } from "../../../../../enums/products";
 
 import Form from "./Form";
 import SubmitButton from "./SubmitButton";
+import { useSuccessMessage } from "../../../../../hooks/useSuccessMessage";
 
 export default function AddForm() {
   const { handleAddProduct, newProduct, setNewProduct } =
     useContext(OrderContext);
 
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const { isSubmitted, displaySuccessMessage } = useSuccessMessage();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,14 +27,7 @@ export default function AddForm() {
 
     handleAddProduct(newProductToAdd);
     setNewProduct(EMPTY_PRODUCT);
-    displaySucessMessage();
-  };
-
-  const displaySucessMessage = () => {
-    setIsSubmitted(true);
-    setTimeout(() => {
-      setIsSubmitted(false);
-    }, 2000);
+    displaySuccessMessage();
   };
 
   return (
